cd5bc977eeffb19b13b856312218f55dc1f6d74e
hundredvisionsguy <winikkc@hsd.k12.or.us>
2017-05-10 17:19:46 -0700

    adding new unit tests for new variant

--- TestCaughtSpeeding.py
+++ TestCaughtSpeeding.py
@@ -6,66 +6,71 @@ import caughtSpeeding
 
 class KnownValues(unittest.TestCase):
 
-    def test_caughtSpeedingFor60NoBirthday(self):
+    def test_caughtSpeedingFor5over_NoBirthday(self):
         # Capture the results of the function
-        result = caughtSpeeding.caughtSpeeding(60, False)
+        result = caughtSpeeding.caughtSpeeding(45, 45, False)
         # Check for expected output
         self.assertEqual(0, result)
 
-    def test_caughtSpeedingFor61NoBirthday(self):
+    def test_caughtSpeedingFor6over_NoBirthday(self):
         # Capture the results of the function
-        result = caughtSpeeding.caughtSpeeding(61, False)
+        result = caughtSpeeding.caughtSpeeding(51, 45, False)
         # Check for expected output
         self.assertEqual(500, result)
 
-    def test_caughtSpeedingFor65NoBirthday(self):
+    def test_caughtSpeedingFor10over_NoBirthday(self):
         # Capture the results of the function
-        result = caughtSpeeding.caughtSpeeding(65, False)
+        result = caughtSpeeding.caughtSpeeding(55, 45, False)
         # Check for expected output
         self.assertEqual(500)
 
-    def test_caughtSpeedingFor65Birthday(self):
+    def test_caughtSpeedingFor21over_NoBirthday(self):
         # Capture the results of the function
-        result = caughtSpeeding.caughtSpeeding(65, True)
-        # Check for expected output
-        self.assertEqual(0, result)
-
-
-    def test_caughtSpeedingFor80NoBirthday(self):
-        # Capture the results of the function
-        result = caughtSpeeding.caughtSpeeding(80, False)
-        # Check for expected output
-        self.assertEqual(500, result)
-
-    def test_caughtSpeedingFor81NoBirthday(self):
-        # Capture the results of the function
-        result = caughtSpeeding.caughtSpeeding(81, False)
+        result = caughtSpeeding.caughtSpeeding(61, 40, False)
         # Check for expected output
         self.assertEqual(1000, result)
 
-    def test_caughtSpeedingFor85NoBirthday(self):
+    def test_caughtSpeedingFor45over_NoBirthday(self):
         # Capture the results of the function
-        result = caughtSpeeding.caughtSpeeding(85, False)
+        result = caughtSpeeding.caughtSpeeding(100, 55, False)
         # Check for expected output
-        self.assertEqual(1000)
+        self.assertEqual(1500, result)
 
-    def test_caughtSpeedingFor85NoBirthday(self):
+    def test_caughtSpeedingFor26over_NoBirthday(self):
         # Capture the results of the function
-        result = caughtSpeeding.caughtSpeeding(85, True)
+        result = caughtSpeeding.caughtSpeeding(81, 55, False)
         # Check for expected output
-        self.assertEqual(500, result)
+        self.assertEqual(1500, result)
 
-    def test_caughtSpeedingFor91NoBirthday(self):
+    def test_caughtSpeedingForOver100_NoBirthday(self):
         # Capture the results of the function
-        result = caughtSpeeding.caughtSpeeding(91, False)
+        result = caughtSpeeding.caughtSpeeding(105, 55, False)
         # Check for expected output
-        self.assertEqual(1000)
+        self.assertEqual(2000)
 
-    def test_caughtSpeedingFor55Birthday(self):
+    def test_caughtSpeedingFor10over_Birthday(self):
         # Capture the results of the function
-        result = caughtSpeeding.caughtSpeeding(55, True)
+        result = caughtSpeeding.caughtSpeeding(65, 55, True)
         # Check for expected output
         self.assertEqual(0, result)
+
+    def test_caughtSpeedingFor20over_Birthday(self):
+        # Capture the results of the function
+        result = caughtSpeeding.caughtSpeeding(60, 40, True)
+        # Check for expected output
+        self.assertEqual(500)
+
+    def test_caughtSpeedingFor25over_Birthday(self):
+        # Capture the results of the function
+        result = caughtSpeeding.caughtSpeeding(65, 40, True)
+        # Check for expected output
+        self.assertEqual(1000, result)
+
+    def test_caughtSpeedingFor103_Birthday(self):
+        # Capture the results of the function
+        result = caughtSpeeding.caughtSpeeding(103, 55, True)
+        # Check for expected output
+        self.assertEqual(1500, result)
 
 # Run the tests
 if __name__ == '__main__':
